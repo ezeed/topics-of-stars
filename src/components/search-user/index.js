3955f01fe58515a25fb5694d76a94e63bc47f2c4
Ezequiel Cabrera <ezcabrera@despegar.com>
2019-08-18 16:28:32 -0300

    Landing and search

---
+++ src/components/search-user/index.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from "react";
+import { UserContext } from "../../context/user";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const SearchUser = ({ size, color }) => {
+  const { username, updateUsername } = useContext(UserContext);
+  const [localUsername, setLocalUsername] = useState(username);
+
+  return (
+    <div className="field has-addons">
+      <div className="control has-icons-left">
+        <input
+          className={`input ${color} ${size}`}
+          type="text"
+          placeholder="GitHub User"
+          value={localUsername}
+          onChange={event => setLocalUsername(event.target.value)}
+        />
+        <span className="icon is-medium is-left">
+          <FontAwesomeIcon icon={faGithub} />
+        </span>
+      </div>
+      <div className="control">
+        <button
+          onClick={() => updateUsername(localUsername)}
+          className={`button ${color} ${size}`}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+SearchUser.defaultProps = {
+  size: "is-large",
+  color: "is-black"
+};
+
+export default SearchUser;
