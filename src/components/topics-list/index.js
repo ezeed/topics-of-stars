880b6b34d8ef29b55110960c0c6771b6d28f33b3
Ezequiel Cabrera <ezcabrera@despegar.com>
2019-08-28 00:22:19 -0300

    Fix #24 - show/hide and don't loose selected topic

--- src/components/topics-list/index.js
+++ src/components/topics-list/index.js
@@ -1,15 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+import { faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
 
 import { TopicContext } from "../../context/topic";
 import Tag from "../tag";
 
 const TopicsList = ({ topics }) => {
   const { selectedTopic, updateSelectedTopic } = useContext(TopicContext);
+  const [showTopics, updateShowTopics] = useState(true);
+  useEffect(() => {
+    //hide if selectedTopic !== empty
+    updateShowTopics(selectedTopic !== "");
+  }, [selectedTopic]);
   return (
     <div className="container is-fluid">
-      <div className={`collapsible ${selectedTopic !== "" ? "collapsed" : ""}`}>
+      <div className={`collapsible ${showTopics ? "collapsed" : ""}`}>
         <div className="field is-grouped is-grouped-multiline">
           {topics.map(currentTopic => (
             <Tag key={currentTopic} topic={currentTopic} />
@@ -17,15 +22,34 @@ const TopicsList = ({ topics }) => {
         </div>
       </div>
       <div style={{ textAlign: "center" }}>
-        {selectedTopic !== "" ? (
+        {showTopics ? (
           <button
-            onClick={() => updateSelectedTopic("")}
-            className="button is-fullwidth is-rounded is-black is-inverted"
+            onClick={() => updateShowTopics(!showTopics)}
+            className="button is-white is-large is-rounded is-fullwidth is-text"
           >
             <FontAwesomeIcon icon={faSortDown} />
           </button>
-        ) : null}
+        ) : (
+          <button
+            onClick={() => updateShowTopics(!showTopics)}
+            className="button is-white is-large is-rounded is-fullwidth is-text"
+          >
+            <FontAwesomeIcon icon={faSortUp} />
+          </button>
+        )}
       </div>
+      {selectedTopic !== "" && (
+        <div style={{ textAlign: "center" }}>
+          <span className="tag is-primary color-is-black is-large">
+            #{selectedTopic}
+            <button
+              style={{ backgroundColor: "black" }}
+              onClick={() => updateSelectedTopic("")}
+              className="delete"
+            ></button>
+          </span>
+        </div>
+      )}
     </div>
   );
 };
